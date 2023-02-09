a1669d759f90f4dfcbedffd4b890eca9e91497b8
luis <102092058+Lhdz21@users.noreply.github.com>
2023-02-09 15:51:52 -0500

    Organize functions inside one code
    re asing the computerChoice variable to contain the final result from gameOptions variable
    put together all the code inside the function playRound

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-//create a function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-function getComputerChoice () {
-    const computerChoice = ['Rock','Paper','scissors'];
-    let index = Math.floor(Math.random() * 3);
-    console.log(`${computerChoice.at(index)}`);
-};
-
-//create a function where the player will select between ‘Rock’, ‘Paper’ or ‘Scissors’.
-function getPlayerSelection () {
-    const playerSelection = prompt('choose between rock paper or scissors',);
-    console.log(playerSelection);
-};
-
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
 function playRound () {
+    let playerSelection = prompt('choose between rock paper or scissors',);
+    console.log(playerSelection);
+
+    let gameOptions = ['rock','paper','scissors'];
+    let index = Math.floor(Math.random() * 3);
+    let computerChoice= (`${gameOptions.at(index)}`);
+    console.log(computerChoice);
+
     if (computerChoice == playerSelection){
         alert("it's a draw");
-    } else if (computerChoice == 'rock' && playerSelection == 'Scissors' || computerChoice == 'Scissors' && playerSelection == 'paper' || computerChoice          == 'paper' && playerSelection == 'rock') {
+    } else if (computerChoice == 'rock' && playerSelection == 'scissors' 
+            || computerChoice == 'scissors' && playerSelection == 'paper' 
+            || computerChoice == 'paper' && playerSelection == 'rock') {
         alert("computer wins");
-    } else if (playerSelection == 'rock' && computerChoice == 'Scissors' || playerSelection == 'Scissors' && computerChoice == 'paper' || playerSelection        == 'paper' && computerChoice == 'rock') {
-        alert("computer wins");
+    } else if (playerSelection == 'rock' && computerChoice == 'scissors'    
+            || playerSelection == 'scissors' && computerChoice == 'paper' 
+            || playerSelection == 'paper' && computerChoice == 'rock') {
+        alert("you win");
     } else {
         alert("invalid choose");
     }
