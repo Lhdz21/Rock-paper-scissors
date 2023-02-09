d6d44b33f1a728c23046cd1af2fe7dc64f7250a2
luis <102092058+Lhdz21@users.noreply.github.com>
2023-02-08 19:45:18 -0500

    Create the function getPlayerSelection
    this function allow the user input rock, paper or scissors.

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function getComputerChoice () {
     let index = Math.floor(Math.random() * 3);
     console.log(`${computerChoice.at(index)}`);
 };
+
+//create a function where the player will select between ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getPlayerSelection () {
+    const playerSelection = prompt('choose between rock paper or scissors',);
+    console.log(playerSelection);
+};
+
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
