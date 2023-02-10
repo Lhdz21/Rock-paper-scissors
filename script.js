6d542c200607529ffd9039841eb8dfad2ea36419
luis <102092058+Lhdz21@users.noreply.github.com>
2023-02-10 15:28:46 -0500

    Add round counter
    the variables computerCounter, playerCounter and roundCounter will count how many times the computer or the player win.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function startGame () {
-    for (let i = 1; i < 6; i++) { // shows 0, then 1, then 2
+    for (let i = 1; i < 6; i++) {
         alert(`round ${i}`);  
         playRound(); 
 };
 }
 
+let computerCounter = 0;
+let playerCounter = 0;
+let roundCounter = 0;
 
 function playRound () {
     let playerSelection = prompt('choose between rock paper or scissors',);
@@ -14,18 +17,45 @@ function playRound () {
     let index = Math.floor(Math.random() * 3);
     let computerChoice= (`${gameOptions.at(index)}`);
     console.log(computerChoice);
-
+    
     if (computerChoice == playerSelection){
         alert("it's a draw");
+        roundCounter++;
     } else if (computerChoice == 'rock' && playerSelection == 'scissors' 
             || computerChoice == 'scissors' && playerSelection == 'paper' 
             || computerChoice == 'paper' && playerSelection == 'rock') {
         alert("computer wins");
+        computerCounter++;
+        roundCounter++;
     } else if (playerSelection == 'rock' && computerChoice == 'scissors'    
             || playerSelection == 'scissors' && computerChoice == 'paper' 
             || playerSelection == 'paper' && computerChoice == 'rock') {
         alert("you win");
+        playerCounter++;
+        roundCounter++;
     } else {
         alert("invalid choose");
-    }
+        roundCounter++;
+    };
+
+
+
+    if (roundCounter == 5 && computerCounter > playerCounter){
+        alert('COMPUTER WINS');
+        computerCounter = 0;
+        playerCounter = 0;
+        roundCounter = 0;
+    } else if (roundCounter == 5 && computerCounter < playerCounter){
+        alert('YOU WIN');
+        computerCounter = 0;
+        playerCounter = 0;
+        roundCounter = 0;
+    } else if (roundCounter == 5 && computerCounter == playerCounter){
+        alert('IT IS A DRAW');
+        computerCounter = 0;
+        playerCounter = 0;
+        roundCounter = 0;
+    };
+console.log(computerCounter);
+console.log(playerCounter);
 };
