3fd0b5ccfd084522359f8852e13f75f6b52ef3f5
luis <102092058+MalangaCode@users.noreply.github.com>
2023-02-24 13:09:13 -0500

    Add the option to display the round number

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let rockEmoji = String.fromCodePoint(9994,127997);
 let paperEmoji = String.fromCodePoint(9995,127997); 
 let scissorsEmoji = String.fromCodePoint(9996,127997);
-
-
-console.log("\uD83D\uDE00");
-console.log(rockEmoji);
-
 let playerChoice;
+let computerCounter = 0;
+let playerCounter = 0;
+let roundCounter = 0;
+
+const btn = document.querySelectorAll('button.emojiButton');
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+    playerChoice = `${button.id}`;
+    document.getElementById('youChose').textContent = `You Chose`;
+    changePlayerEmoji(button.id);
+    console.log(playerChoice);
+    playRound();
+    });
+
+});;
+
 
 function startGame () {
     for (let i = 1; i < 6; i++) {
@@ -49,24 +60,6 @@ function changePlayerEmoji(x){
     
 }
 
-const btn = document.querySelectorAll('button.emojiButton');
-btn.forEach((button) => {
-    button.addEventListener('click', () => {
-    playerChoice = `${button.id}`;
-    document.getElementById('youChose').textContent = `You Chose`;
-    changePlayerEmoji(button.id);
-    console.log(playerChoice);
-    playRound();
-    });
-
-});;
-
-
-
-let computerCounter = 0;
-let playerCounter = 0;
-let roundCounter = 0;
-
 function playRound () {
   computerSelection = computerChoice();
   playerSelection = playerChoice;
@@ -74,24 +67,28 @@ function playRound () {
     if (computerSelection == playerSelection){
         document.getElementById('roundWinner').textContent = `It's a tie`;
         roundCounter++;
+        document.getElementById('roundNumber').textContent = roundCounter;
+
     } else if (computerSelection == 'rock' && playerSelection == 'scissors' 
             || computerSelection == 'scissors' && playerSelection == 'paper' 
             || computerSelection == 'paper' && playerSelection == 'rock') {
             document.getElementById('roundWinner').textContent = `CPU win this round`;
             computerCounter++;
             roundCounter++;
+            document.getElementById('roundNumber').textContent = roundCounter;
+
     } else if (playerSelection == 'rock' && computerSelection == 'scissors'    
             || playerSelection == 'scissors' && computerSelection == 'paper' 
             || playerSelection == 'paper' && computerSelection == 'rock') {
             document.getElementById('roundWinner').textContent = `You win this round`;
             playerCounter++;
             roundCounter++;
+            document.getElementById('roundNumber').textContent = roundCounter;
+
     } else {
         alert("invalid choose");
         roundCounter++;
     };
-
-
 
     if (roundCounter == 5 && computerCounter > playerCounter){
         alert('COMPUTER WINS');
